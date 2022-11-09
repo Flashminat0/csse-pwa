29668328fc15be413849fa61ce17e050ca35a042
Yasas Lowe <wyasaslowe@gmail.com>
2022-11-09 21:30:27 +0530

    header rework

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -38,14 +38,7 @@ const Appbar = () => {
 							</div>
 						</div>
 
-						<div
-							title='Gluten Free'
-							className='h-10 w-10 rounded-full bg-zinc-200 bg-cover bg-center shadow-inner dark:bg-zinc-800'
-							style={{
-								backgroundImage:
-									'url(https://images.unsplash.com/photo-1612480797665-c96d261eae09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80)',
-							}}
-						/>
+
 					</nav>
 				</div>
 			</header>
