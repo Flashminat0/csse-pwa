411ae7d52bbf0fe714426b67b70ccf79d7bf384d
Yasas Lowe <wyasaslowe@gmail.com>
2022-11-10 09:20:21 +0530

    style review on animal add page and auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,9 @@ const Index = () => {
 	return (
 		<div>
 			{JSON.stringify(token)}
-			<button onClick={setTokenHandler}>Set Token</button>
+			<button
+				className={`bg-blue-500 text-white px-4 py-2 rounded-md`}
+				onClick={setTokenHandler}>Set Token</button>
 
 		</div>
 	);
